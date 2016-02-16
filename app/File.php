8314e576ce6b0426e4655751cf9ce3b8f132739a
brti <andrew.p@protonmail.ch>
2016-02-16 16:06:43 +0000

    Re-factor upload logic.

--- app/File.php
+++ app/File.php
@@ -2,16 +2,34 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class File extends Model
 {
 	protected $fillable = [
 		'name',
 		'original_name',
-		'path',
-		'type',
+		'extension',
+		'mime_type',
 		'size',
 		'expire_date'
 	];
+
+	public static function upload(UploadedFile $file)
+	{
+		$file = static::create([
+			'original_name' => $file->getClientOriginalName(),
+			'extension' => $file->getClientOriginalExtension(),
+			'mime_type' => $file->getMimeType(),
+			'size' => $file->getClientSize(),
+			'expire_date' => Carbon::now()->addDay()
+		]);
+
+		$file->name = sha1($file->id);
+		$file->save();
+
+		return $file;
+	}
 }
--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Controller;
 
 class FileController extends Controller
 {
+
 	protected $request;
 
 	public function __construct(Request $request)
@@ -19,15 +20,7 @@ class FileController extends Controller
 
 	public function postUploadFile()
 	{
-		$file = File::create([
-			'original_name' => $this->request->file('file')->getClientOriginalName(),
-			'extension' => $this->request->file('file')->getClientOriginalExtension(),
-			'mime_type' => $this->request->file('file')->getMimeType(),
-			'size' => $this->request->file('file')->getClientSize()
-		]);
-
-		$file->name = sha1($file->id);
-		$file->save();
+		$file = File::upload($this->request->file('file'));
 
 		return response()->json($file->toArray());
 	}
