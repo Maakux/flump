c57ebd9cdd638baaad0660a016cfcd73e5882752
brti <andrew.p@protonmail.ch>
2016-02-18 13:41:51 +0000

    Update link to file.

--- resources/assets/js/views/File/ViewFile.js
+++ resources/assets/js/views/File/ViewFile.js
@@ -42,7 +42,7 @@ class ViewFile extends React.Component {
 			var file = this.state.file;
 
 			var protocol = window.location.protocol + "//";
-			var host = window.location.host;
+			var host = "f." + window.location.host;
 
 			var fileLink = protocol + host + "/" + file.name;
 
