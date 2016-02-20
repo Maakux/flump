f9ee8e6ac5c72115cf219514d580caa9716b67eb
brti <andrew.j.p101@gmail.com>
2016-02-20 16:58:57 +0000

    Add Google analytics.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -11,6 +11,15 @@
 		<div id="app-root"></div>
 	</body>
 	<footer>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-74093027-1', 'auto');
+  ga('send', 'pageview');
+</script>
 		<script type="text/javascript" src="{{ URL::to('js/app.js') }}"></script>
 	</footer>
 </html>
